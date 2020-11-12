3ef7e37aa590064ae77ab0161adb2590b731bae3
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-11-12 12:55:31 +0100

    Add project files.

---
+++ Open-Lab-10.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_10._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
