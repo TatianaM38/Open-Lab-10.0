3b0f7f012aadfde1fc0342d443e35aa04479d721
tmichalikova231@gmail.com <tmichalikova231@gmail.com>
2020-11-12 12:57:04 +0100

    Done

--- Open-Lab-10.0/Program.cs
+++ Open-Lab-10.0/Program.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_10._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello AppsLab!");
         }
     }
 }
